0dee12715852753f28298724a82ca61401f522cb
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-03-11 16:13:48 +0300

    feat: routes implemented

--- python_basket_points/router.py
+++ python_basket_points/router.py
@@ -1,8 +1,28 @@
 from fastapi import APIRouter
 
+from python_basket_points.logic import (
+    get_clustered_points,
+    clustered_points_to_feature_collection,
+    group_clustered_points,
+)
+
 router = APIRouter()
 
 
 @router.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_roout():
+    return {"hello", "world"}
+
+
+@router.get("/cluster/")
+def cluster(count: int = 500):
+    clustered_points = get_clustered_points(point_count=count)
+    feature_collection = clustered_points_to_feature_collection(clustered_points)
+    return {"data": feature_collection}
+
+
+@router.get("/cluster/group")
+def grouped_cluster(count: int = 500):
+    clustered_points = get_clustered_points(point_count=count)
+    grouped_clusters = group_clustered_points(clustered_points)
+    return {"data": grouped_clusters}
