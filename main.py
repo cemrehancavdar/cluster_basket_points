5312d110cfa2753a537577dc9d4feb7e26144feb
cemrehancavdar <cemrehancavdar@hotmail.com>
2023-03-11 16:11:12 +0300

    feat: route added to fastapi instance and auto-reload on dev.

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from fastapi import FastAPI
 
 from python_basket_points.router import router
 
-app = FastAPI(router=router)
+app = FastAPI()
+app.include_router(router=router)
 
 
 def start():
-    uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", port=5000, log_level="info", reload=True)
